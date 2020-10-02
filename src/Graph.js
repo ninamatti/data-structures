637b75be02c8286d9f762d2df565773c12a4275a
Jack Fowler <OnigiriJack@users.noreply.github.com>
2020-10-02 18:42:27 +0900

    tests passing for contains method

--- src/Graph.js
+++ src/Graph.js
@@ -10,6 +10,22 @@ class Graph {
   addNode(value) {
     this.nodes[value] = value;
   }
+  //removeNode(value) removes a node from graph and returns undefined.
+  removeNode(value) {
+    // delete this.nodes[value];
+  }
+  contains(value) {
+    if (value in this.nodes) {
+      return true;
+    }
+    return false;
+  }
+  /* .removeNode(value) removes a node from graph and returns undefined.
+ .contains(value) returns boolean. Returns true if value is found in graph, false otherwise
+ .addEdge(value1, value2) returns undefined. Create connection between two nodes if they're both present in the graph
+ .removeEdge(value1, value2) returns undefined. Remove connection between two nodes
+ .hasEdge(value1, value2) returns boolean. Returns true if edge exists, false otherwise
+ What are the time complexities? */
 }
 
 /*
