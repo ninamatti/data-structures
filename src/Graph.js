92b9638d64ad9770193d13e70f32aff0efa683e2
Nina Matti <ninadesireematti@gmail.com>
2020-10-02 22:24:39 +0900

    added implementation for Graph basic requirements

--- src/Graph.js
+++ src/Graph.js
@@ -1,3 +1,5 @@
+//const { result } = require("underscore");
+
 class Graph {
   constructor() {
     /**
@@ -10,61 +12,78 @@ class Graph {
 
   addNode(value) {
     this.nodes[value] = [];
+    return undefined;
   }
 
   contains(value) {
-    if (value in this.nodes) {
-      return true;
+    let result = false;
+    if (this.nodes[value]) {
+      result = true;
     }
-    return false;
+    return result;
   }
 
   hasEdge(value1, value2) {
-    if (this.contains(value1) && this.contains(value2)) {
-      if (
-        this.nodes[value1].includes(value2) ||
-        this.nodes[value2].includes(value1)
-      ) {
-        return true;
-      } else {
-        return false;
+    let result = false;
+    let value2WasFound = false;
+    let value1WasFound = false;
+
+    if (this.nodes[value1] && this.nodes[value2]) {
+      for (let edge of this.nodes[value1]) {
+        if (edge === value2);
+        value2WasFound = true;
+      }
+      for (let edge of this.nodes[value2]) {
+        if (edge === value1);
+        value1WasFound = true;
       }
     }
-    return false;
+
+    if (value2WasFound && value1WasFound) {
+      result = true;
+    }
+
+    return result;
   }
 
   addEdge(value1, value2) {
-    if (this.hasEdge(value1, value2)) {
-      return "Invalid node value";
-    } else if (this.contains(value1) && this.contains(value2)) {
-      this.nodes[value1].push(value2);
-      this.nodes[value2].push(value1);
+    if (this.nodes[value1] && this.nodes[value2]) {
+      if (!this.hasEdge(value1, value2)) {
+        this.nodes[value1].push(value2);
+      }
+      if (!this.hasEdge(value2, value1)) {
+        this.nodes[value2].push(value1);
+      }
+      return undefined;
     } else {
       return "Invalid node value";
     }
   }
 
-  removeNode(value) {
-    // check if contains node
-    if (this.contains(value)) {
-      //loop through object check each array and remove instances of value
-      let nodeList = this.nodes;
-      for (let nodeVal in nodeList) {
-        if (nodeList[nodeVal].includes(value)) {
-          this.nodes[nodeVal] = this.nodes[nodeVal].filter(
-            (val) => val !== value
-          );
-        }
-      }
-      delete this.nodes[value];
-    }
-  }
-
   removeEdge(value1, value2) {
     if (this.hasEdge(value1, value2)) {
-      this.nodes[value1] = this.nodes[value1].filter((val) => val !== value2);
-      this.nodes[value2] = this.nodes[value2].filter((val) => val !== value1);
+      for (let i = 0; i < this.nodes[value1]; i++) {
+        if (this.nodes[value1][i] === value2) {
+          this.nodes[value1].splice(i, 1);
+        }
+      }
     }
+    return undefined;
+  }
+
+  removeNode(value) {
+    // delete node form adjacency list
+    delete this.nodes[value];
+    // delete edges referencing removed node
+    for (let key in this.nodes) {
+      let edgeArray = this.nodes[key];
+      for (let i = 0; i < edgeArray.length; i++) {
+        if (edgeArray[i] === value) {
+          edgeArray.splice(i, 1);
+        }
+      }
+    }
+    return undefined;
   }
 }
 
